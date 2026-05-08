ab606f0ee99069d8643eea3d2a6074f4d9ba8439
Andzhela Karpukhina <aakarpukhina@rosatom.ru>
2026-05-08 20:42:34 +0300

    add all

--- src/com/javarush/task/pro/task05/task0511/Solution.java
+++ src/com/javarush/task/pro/task05/task0511/Solution.java
@@ -11,5 +11,20 @@ public class Solution {
 
     public static void main(String[] args) {
         //напишите тут ваш код
+        Scanner scanner = new Scanner(System.in);
+        int number = scanner.nextInt();
+
+        multiArray = new int[number][];
+        for (int i = 0; i < multiArray.length; i++) {
+            multiArray[i] = new int[scanner.nextInt()];
+        }
+
+        for (int i = 0; i < multiArray.length; i++) {
+            for (int j = 0; j < multiArray[i].length; j++) {
+                System.out.print("["+multiArray[i][j] + "] ");
+            }
+            System.out.println();
+        }
+
     }
 }
